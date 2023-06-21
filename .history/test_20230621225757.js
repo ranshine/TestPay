b92be93fdd2be558a71beb7f84eae2658e71e143
suresh <ranshine123@gmail.com>
2023-06-21 22:59:18 +0530

    added

---
+++ .history/test_20230621225757.js
@@ -0,0 +1,6 @@
+
+const test = () => {
+    return (
+        <View></View>
+    )
+}
---
+++ .history/test_20230621225836.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { View } from "react-native"
+const Test = ():JSX.Element => {
+    return (
+        <View></View>
+    )
+}
+export default Test
